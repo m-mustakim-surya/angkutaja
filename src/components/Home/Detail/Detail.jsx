b5998ee97a8d4d770bc926ccb40223e252ae9e71
M. Mustakim Surya <muhammadmustakimsurya@gmail.com>
2022-11-03 12:52:38 +0700

    fetching data di page detail

--- src/components/Home/Detail/Detail.jsx
+++ src/components/Home/Detail/Detail.jsx
@@ -1,80 +1,59 @@
-import React from 'react'
 import { useLazyQuery, useQuery } from '@apollo/client';
-import { GetOrders, GetOrderById } from "../../../config/query"
-import Footer from '../../Footer/Footer'
-import Navbar from '../../Navbar/Navbar'
-import './Detail.css'
+import { useState } from 'react';
+import { GetOrders } from "../../../config/query"
+import Footer from '../../Footer/Footer';
+import Navbar from '../../Navbar/Navbar';
+import imgedit from '../../../img/edit.png';
+import imgdelete from '../../../img/delete.png';
+import './Detail.css';
 
-const Detail = ({ deleteOrder, onClickEdit }) => {
-  const { data } = useQuery(GetOrders);
+const Detail = ({ deleteOrder, editOrder }) => {
+    const { data, loading, error, refetch } = useQuery(GetOrders);
 
-  const { data: orderByIdData } = useLazyQuery(GetOrderById);
-  
+    const [passengerId, setPassengerId] = useState("")
 
-  return (
-    <div>
-      <Navbar />
-      <div className='detail'>
-        <h1>Detail Order</h1>
-      </div>
-      <table>
-        <thead>
-          <tr>
-            <th>Nama</th>
-            <th>Jenis angkut</th>
-            <th>Jenis mobil</th>
-            <th>Total jarak</th>
-            <th>Harga</th>
-            <th></th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {orderByIdData ? orderByIdData.miniproject_detail?.map(({ order_id, user_name, jenis_angkut, jenis_mobil, jarak, harga }) => (
-            <tr key={order_id}>
-              <td>{user_name}</td>
-              <td>{jenis_angkut}</td>
-              <td>{jenis_mobil}</td>
-              <td>{jarak} km</td>
-              <td className='harga'>Rp.{harga}</td>
-              <td
-                onClick={() => onClickEdit({order_id, user_name, jenis_angkut, jenis_mobil, jarak, harga})}
-                >
-                  <button>Edit</button>
-              </td>
-              <td
-                onClick={() => deleteOrder(order_id)}
-                >
-                  <button>Delete</button>
-              </td>
-            </tr>
-
-          )
-          ) : data.miniproject_detail.map(({ order_id, user_name, jenis_angkut, jenis_mobil, jarak, harga }) => (
-            <tr key={order_id}>
-              <td>{user_name}</td>
-              <td>{jenis_angkut}</td>
-              <td>{jenis_mobil}</td>
-              <td>{jarak} km</td>
-              <td className='harga'>Rp.{harga}</td>
-              <td
-                onClick={() => onClickEdit({order_id, user_name, jenis_angkut, jenis_mobil, jarak, harga})}
-                >
-                  <button>Edit</button>
-              </td>
-              <td
-                onClick={() => deleteOrder(order_id)}
-                >
-                  <button>Delete</button>
-              </td>
-            </tr>
-          )
-          )}
-        </tbody>
-      </table>
-      <Footer />
-    </div>
-  )
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :(</p>;
+    return (
+        <div>
+          <Navbar />
+          <div className='detail'>
+            <h1>Detail Order</h1>
+          </div>
+          <table>
+              <thead>
+                <tr>
+                  <th>Nama</th>
+                  <th>Jenis angkut</th>
+                  <th>Jenis mobil</th>
+                  <th>Total jarak</th>
+                  <th>Harga</th>
+                  <th></th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                {data.miniproject_detail.map(({ order_id, user_name, jenis_angkut, jenis_mobil, jarak, harga }) => (
+                    <tr key={order_id}>
+                        <td>{user_name}</td>
+                        <td>{jenis_angkut}</td>
+                        <td>{jenis_mobil}</td>
+                        <td>{jarak} km</td>
+                        <td className='harga'>Rp.{harga}</td>
+                        <td onClick={() => editOrder({order_id, user_name, jenis_angkut, jenis_mobil, jarak, harga})}>
+                          <button className='editButton'><img src={imgedit} alt="edit button" /></button>
+                        </td>
+                        <td onClick={() => deleteOrder(order_id)}>
+                          <button className='deleteButton'><img src={imgdelete} alt="delete button" /></button>
+                        </td>
+                    </tr>
+                )
+                )}
+              </tbody>
+          </table>
+          <Footer />
+        </div>
+    )
 }
 
-export default Detail
+export default Detail;
--- src/config/query.jsx
+++ src/config/query.jsx
@@ -1,18 +1,5 @@
 import { gql } from "@apollo/client";
 
-export const GetOrderById = gql`
-query GetOrderById($id: uuid) {
-  miniproject_detail(where: {order_id: {_eq: $id}}) {
-    order_id
-    user_name
-    jenis_angkut
-    jenis_mobil
-    jarak
-    harga
-  }
-}
-
-`;
 export const GetOrders = gql`
 query GetOrders {
   miniproject_detail {
